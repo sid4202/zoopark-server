8504695d64c809af6d002a59bfddc03d845ad83c
blackwhitebear <maxonleb5@gmail.com>
2022-04-10 14:46:01 +0300

    fix

--- server_with_correct_structure/test/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once ("src/Services/RequestHandler.php");
-$requestHandler = new RequestHandler();
-
-$requestHandler->handle();
--- server_with_correct_structure/test/src/Services/Database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Database
-{
-    public static function getConnection()
-    {
-        return mysqli_connect('localhost', 'root', 'Cra5hLoy@ale', 'creatures');
-    }
-}
