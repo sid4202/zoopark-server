4676246827ef0c929469eb1508af471561016bc5
blackwhitebear <maxonleb5@gmail.com>
2022-04-17 03:02:23 +0300

    tests reworked

--- tests.py
+++ tests.py
@@ -1,19 +1,65 @@
 import requests
 
-text = requests.get('http://localhost:8000/animals').text
-if text == "[]":
-    print("OK")
-else:
-    print("Not OK")
 
-text = requests.post('http://localhost:8000/animal', '{"name":"Melman","type":"giraffe"}').text
-if text:
-    print("OK")
-else:
-    print("Not OK")
+def empty_check():
+    text = requests.get('http://localhost:8000/animals').text
+    if text == "[]":
+        return True
+    else:
+        print(text)
+        return False
 
-text = requests.get('http://localhost:8000/animal/1').text
-if text != "null":
-    print("OK")
-else:
-    print("Not OK")
+
+def post_animal_check():
+    text = requests.post('http://localhost:8000/animal', '{"name":"Melman","type":"giraffe"}').text
+    if text:
+        return True
+    else:
+        print(text)
+        return False
+
+
+def get_animal_check():
+    text = requests.get('http://localhost:8000/animal/1').text
+    if text != "null":
+        return True
+    else:
+        print(text)
+        return False
+
+
+def wrong_animal_check():
+    text = requests.get('http://localhost:8000/animal/1927353').text
+    if text != "null":
+        return True
+    else:
+        print(text)
+        return False
+
+
+def post_weirdName_animal():
+    text = requests.post('http://localhost:8000/animal', '{"name":"Melman","type":"/+=-&giraffe"}').text
+    if text:
+        return True
+    else:
+        print(text)
+        return False
+
+
+def correct_deletion_check():
+    text = requests.delete('http://localhost:8000/animal/1').text
+    if text == "deleted":
+        return True
+    else:
+        print(text)
+        return False
+
+
+arr = [empty_check, post_animal_check, wrong_animal_check, post_weirdName_animal, correct_deletion_check]
+
+for test in arr:
+    result = test()
+    if result:
+        print("ok")
+    else:
+        print("not ok")
